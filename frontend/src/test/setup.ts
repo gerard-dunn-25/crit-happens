fefd42b3c5191ae844380d88533f93326a4d393d
Gerard Dunn <gerard.dunn.92@gmail.com>
2026-03-05 20:00:41 +1300

    updated test setup to account for ResizeObserver

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom'
+import { vi } from 'vitest'
+
+const ResizeObserverMock = class {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+vi.stubGlobal('ResizeObserver', ResizeObserverMock)
